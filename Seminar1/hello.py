0e2063e1897b68afd7c1aaa5769b43ad1c775698
IrinaK <skorpya@yandex.ru>
2022-11-16 16:03:04 +0300

    seminar

--- Seminar1/hello.py
+++ Seminar1/hello.py
@@ -1,1 +1,10 @@
-print("hello world")
+print("hello world") #string
+print(6) #integer
+a=b=10
+a=a+1
+print(b) #10
+a=int(input("Введите а "))
+if a%2==0:
+    print(True) #четное
+else:
+    print(False) #нечетное
