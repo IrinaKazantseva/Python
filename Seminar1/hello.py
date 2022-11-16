3130fdb6b6b04dcb64f8d79960606dbc441ad4f2
IrinaK <skorpya@yandex.ru>
2022-11-16 18:26:01 +0300

    task3

--- Seminar1/hello.py
+++ Seminar1/hello.py
@@ -1,13 +1,17 @@
 #print("hello world") #string
+
 #print(6) #integer
+
 #a=b=10
 #a=a+1
 #print(b) #10
+
 #a=int(input("Введите а "))
 #if a%2==0:
     #print(True) #четное
 #else:
     #print(False) #нечетное
+
 #1. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
 #*Примеры:*
 #- 5, 25 -> да
@@ -46,9 +50,49 @@
 #     maks = e
 # print("Максимальное число ", maks)
 
-numbers=[int(input("Введите первое число ")), int(input("Введите второе число ")), int(input("Введите третье число ")), int(input("Введите четвертое число ")), int(input("Введите пятое число "))]
-maks=numbers[0]
-for i in range(len(numbers)):
-    if numbers[i]>maks:
-        maks=numbers[i]
-print(maks)
+# list_number = []
+# for i in range(5):
+# a=int(input())
+# list_number.append(a)
+# print(list_number)
+# max=list_number[0]
+# for i in range(len(list_number)):
+# if list_number[i]>max:
+# max=list_number[i]
+# print(max)
+
+#numbers=[int(input("Введите первое число ")), int(input("Введите второе число ")), int(input("Введите третье число ")), int(input("Введите четвертое число ")), int(input("Введите пятое число "))]
+# maks=numbers[0]
+# for i in range(len(numbers)):
+#     if numbers[i]>maks:
+#         maks=numbers[i]
+# print(maks)
+#print(max(numbers))  # функция
+
+# 1. Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N
+#
+# *Примеры:*
+#
+# - 5 -> -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
+
+# n=int(input("Введите число N: "))
+# numbers = []
+# start = -n
+# for i in range(n*2+1):
+#     numbers.append(start)
+#     start+=1
+# print(numbers)
+
+n = int(input("Введите число N: "))
+m = -n
+while n >= m:
+    print(m)
+    m += 1
+
+# 2. Напишите программу, которая будет принимать на вход дробь и показывать первую цифру дробной части числа.
+#
+# *Примеры:*
+#
+# - 6,78 -> 7
+# - 5 -> нет
+# - 0,34 -> 3
