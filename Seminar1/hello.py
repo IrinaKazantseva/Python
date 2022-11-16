36f1053157ebd411853fc2ce78a80220544b26c9
IrinaK <skorpya@yandex.ru>
2022-11-16 17:55:02 +0300

    task2

--- Seminar1/hello.py
+++ Seminar1/hello.py
@@ -1,27 +1,27 @@
-print("hello world") #string
-print(6) #integer
-a=b=10
-a=a+1
-print(b) #10
-a=int(input("Введите а "))
-if a%2==0:
-    print(True) #четное
-else:
-    print(False) #нечетное
+#print("hello world") #string
+#print(6) #integer
+#a=b=10
+#a=a+1
+#print(b) #10
+#a=int(input("Введите а "))
+#if a%2==0:
+    #print(True) #четное
+#else:
+    #print(False) #нечетное
 #1. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
 #*Примеры:*
 #- 5, 25 -> да
 #- 4, 16 -> да
 #- 25, 5 -> да
 #- 8,9 -> нет
-a=int(input("Введите первое число "))
-b=int(input("Введите второе число "))
-if b==a*a:
-    print("второе число - квадрат первого")
-elif a==b*b:
-    print("первое число - квадрат второго")
-else:
-    print("числа не квадраты друг друга")
+#a=int(input("Введите первое число "))
+#b=int(input("Введите второе число "))
+#if b==a*a:
+    #print("второе число - квадрат первого")
+#elif a==b*b:
+    #print("первое число - квадрат второго")
+#else:
+    #print("числа не квадраты друг друга")
 
 #2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
 
@@ -29,3 +29,26 @@ else:
 
 #- 1, 4, 8, 7, 5 -> 8
 #- 78, 55, 36, 90, 2 -> 90
+
+# a=int(input("Введите первое число "))
+# b=int(input("Введите второе число "))
+# c=int(input("Введите третье число "))
+# d=int(input("Введите четвертое число "))
+# e=int(input("Введите пятое число "))
+# maks=a
+# if b>maks:
+#     maks=b
+# if c > maks:
+#     maks = c
+# if d > maks:
+#     maks = d
+# if e > maks:
+#     maks = e
+# print("Максимальное число ", maks)
+
+numbers=[int(input("Введите первое число ")), int(input("Введите второе число ")), int(input("Введите третье число ")), int(input("Введите четвертое число ")), int(input("Введите пятое число "))]
+maks=numbers[0]
+for i in range(len(numbers)):
+    if numbers[i]>maks:
+        maks=numbers[i]
+print(maks)
