2bf5bff123a55af349ad79afb90bff3d6c2c5078
IrinaK <skorpya@yandex.ru>
2022-11-20 23:35:56 +0300

    facultative

--- Seminar2/file_txt_position.py
+++ Seminar2/file_txt_position.py
@@ -2,11 +2,36 @@
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
 # Реализуйте алгоритм перемешивания списка.
+import random
 
+# задаем список (верно для N - положительное число)
 n = int(input("Введите число N: "))
-numbers = list(range(-n, n+1)) #верно для положительных чисел
-print(numbers)
+numbers = list(range(-n, n+1))
+print(f"Cписок: \n {numbers}")
 
+#перемешиваем
+random.shuffle(numbers)
+print(f"Новый список: \n {numbers}")
+
+# создаем файл для хранения позиций
+#import random
 with open('file.txt', 'w') as file:
     for i in range(n*2+1):
-        file.write(f'{i}\n')
+        file.write(f'{i}\n') # позиции в возрастающем порядке
+        #file.write(f"{random.randrange(0, 2*n)}\n") # рандомное заполнение
+
+# записываем позиции в список
+with open('file.txt', 'r') as file:
+    positions = list(map(int,file.readlines()))
+#print(positions)
+
+# указываем номера позиций
+print(f"Диапазон позиций: от 0 до {n*2}")
+position1 = positions[int(input("Введите позицию первого множителя: "))]
+position2 = positions[int(input("Введите позицию второго множителя: "))]
+
+# находим произведение элементов на указанных позициях
+multiply = numbers[position1]*numbers[position2]
+
+print(f'Значение множителя с индексом {position1} равно {numbers[position1]}. Значение множителя с индексом {position2} равно {numbers[position2]}. Их произведение = {multiply}')
+
