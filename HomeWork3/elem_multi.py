f297047c0981195c436f0a77e521e56fd5514e79
IrinaK <skorpya@yandex.ru>
2022-11-24 23:22:49 +0300

    task2

--- HomeWork3/elem_multi.py
+++ HomeWork3/elem_multi.py
@@ -3,3 +3,13 @@
 # Пример:
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+numbers = list(map(int, input("Введите числа через пробел: ").split()))
+multiply = []
+if len(numbers)%2 == 0:
+   for i in range(len(numbers) // 2):
+      multiply.append(numbers[i] * numbers[len(numbers) - 1 - i])
+else:
+   for i in range(len(numbers)//2+1):
+      multiply.append(numbers[i]*numbers[len(numbers)-1-i])
+print(multiply)
