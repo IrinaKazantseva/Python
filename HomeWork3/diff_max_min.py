e2a61c046054693a262f47b7d6a7bbdba66c1459
IrinaK <skorpya@yandex.ru>
2022-11-24 23:50:05 +0300

    task3

--- HomeWork3/diff_max_min.py
+++ HomeWork3/diff_max_min.py
@@ -3,4 +3,9 @@
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-
+numbers = list(map(float, input("Введите числа через пробел: ").split()))
+newnumbers = []
+for i in range(len(numbers)):
+    if numbers[i] % 1 != 0:
+        newnumbers.append(round(numbers[i]%1, 2))
+print(f"Разница между максимальным и минимальным значением дробной части элементов списка = {max(newnumbers) - min(newnumbers)}")
