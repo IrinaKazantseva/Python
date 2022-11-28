846c8d8010853c25fce97d52d3793f1978456baa
IrinaK <skorpya@yandex.ru>
2022-11-29 00:49:52 +0300

    task3

--- HomeWork4/unrepeat.py
+++ HomeWork4/unrepeat.py
@@ -1,2 +1,11 @@
 # Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+numbers = list(map(int, input("Введите последовательность чисел через пробел: ").split()))
+unique_numbers = []
+for number in numbers:
+    if number in unique_numbers:
+        continue
+    else:
+        unique_numbers.append(number)
+print(unique_numbers)
