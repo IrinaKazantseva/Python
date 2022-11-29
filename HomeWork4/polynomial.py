f237f789da8be445ee4947c06c99a79e48306492
IrinaK <skorpya@yandex.ru>
2022-11-29 14:59:45 +0300

    task5formal

--- HomeWork4/polynomial.py
+++ HomeWork4/polynomial.py
@@ -9,29 +9,29 @@ k = int(input("Задайте натуральную степень k = "))
 koeflist = [random.randint(0, 101) for i in range(k+1)]
 
 list = koeflist[::-1]
-str=""
+stroka=""
 if len(list) < 1:
-    str = 'x = 0'
+    stroka = 'x = 0'
 else:
     for i in range(len(list)):
         if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
-            str += f'{list[i]}x^{len(list) - i - 1}'
+            stroka += f'{list[i]}x^{len(list) - i - 1}'
             if list[i + 1] != 0:
-                str += ' + '
+                stroka += ' + '
         elif i == len(list) - 2 and list[i] != 0:
-            str += f'{list[i]}x'
+            stroka += f'{list[i]}x'
             if list[i + 1] != 0:
-                str += ' + '
+                stroka += ' + '
         elif i == len(list) - 1 and list[i] != 0:
-            str += f'{list[i]} = 0'
+            stroka += f'{list[i]} = 0'
         elif i == len(list) - 1 and list[i] == 0:
-            str += ' = 0'
-print(str)
+            stroka += ' = 0'
+print(stroka)
 
-with open('polynom1.txt', 'w') as data:
-    data.write(str)
+# with open('polynom1.txt', 'w') as data:
+#     data.write(stroka)
 #
 # Для следующей задачи:
-# with open('polynom2.txt', 'w') as data:
-#     data.write(str)
+with open('polynom2.txt', 'w') as data:
+    data.write(stroka)
 
--- HomeWork4/polynomial_sum.py
+++ HomeWork4/polynomial_sum.py
@@ -1,4 +1,11 @@
 # Даны два файла, в каждом из которых находится запись многочлена. 
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-
+with open('polynom1.txt', 'r') as data:
+    stroka1 = data.readlines()
+with open('polynom2.txt', 'r') as data:
+    stroka2 = data.readlines()
+print(f"Первый многочлен: {stroka1}")
+print(f"Второй многочлен: {stroka2}")
+with open('polynom3.txt', 'w') as data:
+    data.write(f"{stroka1} + {stroka2}")
