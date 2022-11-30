a17593d24a78b2d7bb2df37837d6b5ef2c814af1
IrinaK <skorpya@yandex.ru>
2022-11-30 20:38:06 +0300

    sem5

--- HomeWork3/numeric_sys_change.py
+++ HomeWork3/numeric_sys_change.py
@@ -3,11 +3,13 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
-
-decimal = int(input("Введите десятичное число: "))
-binar = ""
-while decimal > 0:
-    binar = str(decimal%2)+binar
-    decimal//=2
-print(binar)
-
+#
+# decimal = int(input("Введите десятичное число: "))
+# binar = ""
+# while decimal > 0:
+#     binar = str(decimal%2)+binar
+#     decimal//=2
+# print(binar)
+#
+a=int(input('Введите число = '))
+print(bin(a)[2:])
