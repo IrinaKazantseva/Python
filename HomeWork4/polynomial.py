bb6e9087c2778e30ee00e07ee0db9e2692eb5aef
IrinaK <skorpya@yandex.ru>
2022-12-05 16:05:46 +0300

    task sem6

--- HomeWork4/polynomial.py
+++ HomeWork4/polynomial.py
@@ -35,3 +35,52 @@ print(stroka)
 with open('polynom2.txt', 'w') as data:
     data.write(stroka)
 
+
+# from random import randint
+#
+# k = int(input('Insert equation power: '))
+# koefs = list()
+# for i in range(1, k + 2):
+# koefs.append(randint(1, 100))
+#
+# ans = list()
+# for i in range(len(koefs)):
+# if k == 1:
+# ans.append(f'{koefs[i]}*x')
+# elif k == 0:
+# ans.append(f'{koefs[i]}')
+# else:
+# ans.append(f'{koefs[i]}*x**{k}')
+# flag = randint(0, 1)
+# if flag == 1:
+# ans.append('+')
+# elif flag == 0:
+# ans.append('-')
+# k -= 1
+#
+# ans.pop(-1)
+# ans.append('=0')
+# fout = open('output.txt', 'w')
+# fout.write(''.join(ans))
+# fout.close()
+
+# подключение библиотеки
+# import random
+# from numpy.polynomial import Polynomial as P
+#
+# p = P([0, 0, -2,])
+#
+# print(p)
+#
+# k = random.randint(1, 4)
+# print(k)
+# poly = p ** k
+# print(poly)
+#
+# data = open('file04', 'w')
+# data.write(str(poly))
+# data.close()
+#
+# data = open('file04', 'r')
+# print(*data)
+# data.close()
