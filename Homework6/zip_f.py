91b824ec7401afa717f1beb3baefaf67a7b45268
IrinaK <skorpya@yandex.ru>
2022-12-05 21:41:30 +0300

    hw6

---
+++ Homework6/zip_f.py
@@ -0,0 +1,12 @@
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+number = input("Задайте номер четверти: ")
+
+coord = ['x > 0 and y > 0', 'x < 0 and y > 0', 'x < 0 and y < 0', 'x > 0 and y < 0']
+quarter = ['1', '2', '3', '4']
+
+data = list(zip(quarter, coord))
+print(data)
+
+for i in range(4):
+    if number in data[i][0]:
+        print(data[i][1])
