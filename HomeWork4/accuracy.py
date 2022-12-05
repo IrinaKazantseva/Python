a6491c25fb5178fe215d5db3a85a0d227b759c3a
IrinaK <skorpya@yandex.ru>
2022-12-05 15:52:09 +0300

    task sem6

--- HomeWork4/accuracy.py
+++ HomeWork4/accuracy.py
@@ -6,3 +6,20 @@ from math import pi
 d = int(input("Задайте точность числа Пи: "))
 print(f'Число Пи равно {round(pi, d)}')
 
+# import math
+# d = input('Введите число d указывающее степень округления числа pi ')
+# d = d[2:len(d)]
+# print(round(math.pi,len(d)))
+
+# a = int(input('введите нужную точность 1#= '))
+# pi_target = 0
+# for i in range(1, 1000000):
+# pi_target += 4 * ((-1) ** (i + 1)) / (2 * i - 1)
+# print(str(pi_target)[:a + 2])
+
+# number = float(input())
+# d = list('0.001')
+# d.reverse()
+# h = d.index('.')
+# print(h)
+# print(round(number, h))
--- HomeWork4/unrepeat.py
+++ HomeWork4/unrepeat.py
@@ -1,11 +1,37 @@
 # Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
-numbers = list(map(int, input("Введите последовательность чисел через пробел: ").split()))
-unique_numbers = []
-for number in numbers:
-    if number in unique_numbers:
-        continue
-    else:
-        unique_numbers.append(number)
-print(unique_numbers)
+# numbers = list(map(int, input("Введите последовательность чисел через пробел: ").split()))
+# unique_numbers = []
+# for number in numbers:
+#     if number in unique_numbers:
+#         continue
+#     else:
+#         unique_numbers.append(number)
+# print(unique_numbers)
+
+numbers = list(map(int, input("Введите числа через пробел:\n").split()))
+print(numbers)
+new_numbers = []
+for i in numbers:
+if i not in new_numbers:
+new_numbers.append(i)
+print(new_numbers)
+
+# def elements(nums):
+# nums = [int(i) for i in nums.split()]
+# return list(set(nums))
+# numbers = '1 1 2 2 3 455 66 66 2 1'
+# print(elements(numbers))
+
+# для другого типа задач!
+# a= [1,2,2,2,2,3,1,4]
+# print(set(a))
+
+# b = [1, 1, 2, 3, 3, 4, 5]
+# a = []
+# for i in b:
+# if b.count(i) == 1:
+# a.append(i)
+# print(a)
+
