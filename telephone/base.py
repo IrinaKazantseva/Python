cb38057c9c7da1b4fa5e93df8ebb8dcdf8598504
IrinaK <skorpya@yandex.ru>
2022-12-09 01:50:44 +0300

    telbase

--- telephone/base.py
+++ telephone/base.py
@@ -18,31 +18,31 @@ def notes():
     return note
 
 # Запись в csv
-# def start():
-#     with open('bd.csv', 'w', encoding = 'windows-1251') as file:
-#         string = "ID, Имя, Фамилия, Телефон\n"
-#         file.writelines(string)
-#         for i in range(10):
-#             a = f'{str(i + 1)},{notes()}\n'
-#             file.write(a)
-
-# Запись в txt
 def start():
-    with open('bd.txt', 'w', encoding = 'windows-1251') as file:
+    with open('bd.csv', 'w', encoding = 'windows-1251') as file:
         string = "ID, Имя, Фамилия, Телефон\n"
         file.writelines(string)
         for i in range(10):
             a = f'{str(i + 1)},{notes()}\n'
             file.write(a)
-    # конвертация в csv
-    with open('bd.txt', 'r') as in_file:
-        lines = in_file.read().splitlines()
-        stripped = [line.replace(",", " ").split() for line in lines]
-        grouped = zip(*[stripped] * 1)
-        with open('bd.csv', 'w', newline='') as out_file:
-            writer = csv.writer(out_file)
-            for group in grouped:
-                writer.writerows(group)
+
+# Запись в txt
+# def start():
+#     with open('bd.txt', 'w', encoding = 'windows-1251') as file:
+#         string = "ID, Имя, Фамилия, Телефон\n"
+#         file.writelines(string)
+#         for i in range(10):
+#             a = f'{str(i + 1)},{notes()}\n'
+#             file.write(a)
+#     # конвертация в csv
+#     with open('bd.txt', 'r') as in_file:
+#         lines = in_file.read().splitlines()
+#         stripped = [line.replace(",", " ").split() for line in lines]
+#         grouped = zip(*[stripped] * 1)
+#         with open('bd.csv', 'w', newline='') as out_file:
+#             writer = csv.writer(out_file)
+#             for group in grouped:
+#                 writer.writerows(group)
 
 
 start()
