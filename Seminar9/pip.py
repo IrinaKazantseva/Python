f79aa23d13dbee6e1ff1b4ca8d11af912cf2f22b
IrinaK <skorpya@yandex.ru>
2022-12-19 21:31:20 +0300

    pip

--- Seminar9/pip.py
+++ Seminar9/pip.py
@@ -1,12 +1,5 @@
-# from isOdd import isOdd
+from isOdd import isOdd
 
-# print(isOdd(1)) 
-# print(isOdd(4)) 
+print(isOdd(3))
+print(isOdd(5))
 
-from progress.bar import Bar
-
-bar = Bar('Processing', max=20)
-for i in range(20):
-    # Do some work
-    bar.next()
-bar.finish()
